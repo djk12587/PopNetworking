65d22fdcba3f5f115724f440e4fa629fe4abc5a1
Dan Koza MacBook Pro <danieljkoza@gmail.com>
2021-02-14 17:45:38 -0600

    updating the sample app

--- example/exampleApp/ViewController.swift
+++ example/exampleApp/ViewController.swift
@@ -11,7 +11,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        addInstructionLabel()
 
         API.Jokes.Routes.GetJoke().request { result in
             switch result {
@@ -48,5 +48,23 @@ class ViewController: UIViewController {
                     print(error)
             }
         }
+    }
+
+    private func addInstructionLabel() {
+        view.backgroundColor = .white
+
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.lineBreakMode = .byWordWrapping
+        label.text = "Nothing to see here. Look at your debug console for request responses!"
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+
+        view.addSubview(label)
+        NSLayoutConstraint.activate([
+            label.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            label.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            label.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16)
+        ])
     }
 }
