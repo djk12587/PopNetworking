86e2aff5ecee08ec7a849938fe7c365b3c3ab5bf
Dan Koza <danieljkoza@gmail.com>
2021-02-15 09:32:07 -0600

    moving the example app to a new repository https://github.com/djk12587/PopNetworking-ExampleUsage

--- Package.swift
+++ Package.swift
@@ -6,22 +6,15 @@ import PackageDescription
 let package = Package(
     name: "PopNetworking",
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "PopNetworking",
-            targets: ["PopNetworking"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+            targets: ["PopNetworking"]
+        ),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "PopNetworking",
-            path: "Sources",
-            exclude: ["example"] //This prevents the sample app from being added to your target.
+            path: "Sources"
         )
     ]
 )
