4f02301eb8e182218798287855746111565d1c86
Dan Koza <danieljkoza@gmail.com>
2021-11-11 15:26:51 -0600

    Adding some documentation to URLSessionProtocol

--- Sources/NetworkingSession/NetworkingSession+URLSessionProtocol.swift
+++ Sources/NetworkingSession/NetworkingSession+URLSessionProtocol.swift
@@ -7,6 +7,9 @@
 
 import Foundation
 
+/// URLSession cannot be subclassed, so instead we utilize this protocol.
+///
+/// - Note: `URLSession` adheres to ``URLSessionProtocol``
 public protocol URLSessionProtocol {
     func dataTask(with request: URLRequest, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask
 }
