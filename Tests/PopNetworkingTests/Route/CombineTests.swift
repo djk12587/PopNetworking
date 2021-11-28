4d0fe262368537dc89decfb7c253c32b466c04f2
Dan Koza MacBook Pro <danieljkoza@gmail.com>
2021-11-27 20:55:31 -0600

    finishing combine tests

--- Tests/PopNetworkingTests/Route/CombineTests.swift
+++ Tests/PopNetworkingTests/Route/CombineTests.swift
@@ -11,27 +11,35 @@ import Combine
 
 class CombineTests: XCTestCase {
 
-    func testResultPublisherSuccess() throws {
+    func testPublisherSuccess() {
+        let expectation = expectation(description: "wait for response")
         var cancellables = Set<AnyCancellable>()
-        let mockRoute = Mock.Route<Int>(responseSerializer: Mock.ResponseSerializer(.success(200)))
+
+        let mockRoute = Mock.Route(responseSerializer: Mock.ResponseSerializer(.success("mock_response")))
         mockRoute.publisher.sink { completion in
             XCTAssertEqual(completion, .finished)
+            expectation.fulfill()
         } receiveValue: { result in
             switch result {
                 case .success(let response):
-                    XCTAssertEqual(response, 200)
+                    XCTAssertEqual(response, "mock_response")
                 case .failure:
                     XCTFail("request should not fail")
             }
         }.store(in: &cancellables)
+
+        waitForExpectations(timeout: 5)
     }
 
-    func testResultPublisherFailure() throws {
+    func testPublisherFailure() {
+        let expectation = expectation(description: "wait for response")
         var cancellables = Set<AnyCancellable>()
+
         let mockError = NSError(domain: "mockError", code: 1)
         let mockRoute = Mock.Route<Void>(responseSerializer: Mock.ResponseSerializer(.failure(mockError)))
         mockRoute.publisher.sink { completion in
             XCTAssertEqual(completion, .finished)
+            expectation.fulfill()
         } receiveValue: { result in
             switch result {
                 case .success:
@@ -40,22 +48,31 @@ class CombineTests: XCTestCase {
                     XCTAssertEqual(error as NSError, mockError)
             }
         }.store(in: &cancellables)
+
+        waitForExpectations(timeout: 5)
     }
 
-    func testFailableResultPublisherSuccess() throws {
+    func testFailablePublisherSuccess() {
+        let expectation = expectation(description: "wait for response")
         var cancellables = Set<AnyCancellable>()
-        let mockRoute = Mock.Route<Int>(responseSerializer: Mock.ResponseSerializer(.success(200)))
+
+        let mockRoute = Mock.Route(responseSerializer: Mock.ResponseSerializer(.success("mock_response")))
         mockRoute.failablePublisher.sink { completion in
             if case .failure = completion {
                 XCTFail("request should not fail")
             }
+            expectation.fulfill()
         } receiveValue: { result in
-            XCTAssertEqual(result, 200)
+            XCTAssertEqual(result, "mock_response")
         }.store(in: &cancellables)
+
+        waitForExpectations(timeout: 5)
     }
 
-    func testFailableResultPublisherFailure() throws {
+    func testFailablePublisherFailure() {
+        let expectation = expectation(description: "wait for response")
         var cancellables = Set<AnyCancellable>()
+
         let mockError = NSError(domain: "mockError", code: 1)
         let mockRoute = Mock.Route<Void>(responseSerializer: Mock.ResponseSerializer(.failure(mockError)))
         mockRoute.failablePublisher.sink { completion in
@@ -65,8 +82,28 @@ class CombineTests: XCTestCase {
                 case .finished:
                     XCTFail("request should fail")
             }
+            expectation.fulfill()
         } receiveValue: { result in
             XCTFail("request should fail")
         }.store(in: &cancellables)
+
+        waitForExpectations(timeout: 5)
+    }
+
+    func testCancellation() {
+        var cancellables = Set<AnyCancellable>()
+        let expectation = expectation(description: "wait for response")
+
+        let mockRoute = Mock.Route<String>(session: NetworkingSession())
+        let cancellable = mockRoute.publisher
+            .handleEvents(receiveCancel: {
+                expectation.fulfill()
+            })
+            .sink(receiveValue: { _ in })
+
+        cancellable.store(in: &cancellables)
+        cancellable.cancel()
+
+        waitForExpectations(timeout: 5)
     }
 }
