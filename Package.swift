91bc4f979de3fe23f57576d3a448da6dd115f91d
Dan Koza <danieljkoza@gmail.com>
2022-06-20 11:20:08 -0500

    Attempting to add DocC to github pages

--- Package.swift
+++ Package.swift
@@ -17,6 +17,10 @@ let package = Package(
             targets: ["PopNetworking"]
         ),
     ],
+    dependencies: [
+        // other dependencies
+        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
+    ],
     targets: [
         .target(
             name: "PopNetworking",
