07fa7aceafca24653273cda292244cc8a1e62b18
Dan Koza <danieljkoza@gmail.com>
2022-09-15 13:09:17 -0500

    removing macOS support

--- Sources/NetworkingSession/NetworkingSession+URLSessionProtocol.swift
+++ Sources/NetworkingSession/NetworkingSession+URLSessionProtocol.swift
@@ -11,16 +11,7 @@ import Foundation
 ///
 /// - Note: `URLSession` adheres to ``URLSessionProtocol``
 public protocol URLSessionProtocol {
-    @available(iOS 13.0, tvOS 13.0, watchOS 6.0, *)
     func data(for request: URLRequest) async throws -> (Data, URLResponse)
-
-    @available(macOS 12.0, iOS 15.0, tvOS 15.0, watchOS 8.0, *)
-    func data(for request: URLRequest, delegate: URLSessionTaskDelegate?) async throws -> (Data, URLResponse)
 }
 
-@available(macOS 12.0, iOS 15.0, tvOS 15.0, watchOS 8.0, *)
-extension URLSession: URLSessionProtocol {
-    public func data(for request: URLRequest) async throws -> (Data, URLResponse) {
-        try await data(for: request, delegate: nil)
-    }
-}
+extension URLSession: URLSessionProtocol {}
