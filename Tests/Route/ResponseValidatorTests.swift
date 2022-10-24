9ac294417100259a72e9024b08fcd39f4f328537
Dan Koza MacBook Pro <danieljkoza@gmail.com>
2022-10-24 11:38:52 -0500

    Adding some documentation and unit tests for response validation

---
+++ Tests/Route/ResponseValidatorTests.swift
@@ -0,0 +1,27 @@
+//
+//  ResponseValidatorTests.swift
+//  
+//
+//  Created by Dan Koza on 10/24/22.
+//
+
+import XCTest
+@testable import PopNetworking
+
+class ResponseValidatorTests: XCTestCase {
+
+    func testResponseValidatorFailsRequest() async throws {
+        let mockError = NSError(domain: "failed validation", code: 0)
+        let result = await Mock.Route(responseValidator: Mock.ResponseValidator(mockValidation: .failure(mockError)),
+                                      responseSerializer: Mock.ResponseSerializer(.success("mock_success_response"))).result
+
+        XCTAssertThrowsError(try result.get()) { error in
+            XCTAssertEqual(error as NSError, mockError)
+        }
+    }
+
+    func testResponseValidatorValidatesRequest() async throws {
+        let result = await Mock.Route(responseSerializer: Mock.ResponseSerializer<Void>(.success(Void()))).result
+        XCTAssertNoThrow(try result.get())
+    }
+}
