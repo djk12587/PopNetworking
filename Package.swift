a4a1b08c449d7cf87a3fc57c8883be25ee77c920
Dan Koza <daniel.koza@superpedestrian.com>
2025-06-25 15:56:50 -0500

    updated DocC plugin

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.4.4"),
     ],
     targets: [
         .target(
